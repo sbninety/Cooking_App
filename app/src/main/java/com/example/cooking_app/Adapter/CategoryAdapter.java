52c015e457c3f8e0737b50a844e6d08dc4d02ff9
hoang <hoangdinh3123@gmail.com>
2023-11-08 11:05:48 +0700

    add page recipe by category 2

--- app/src/main/java/com/example/cooking_app/Adapter/CategoryAdapter.java
+++ app/src/main/java/com/example/cooking_app/Adapter/CategoryAdapter.java
@@ -3,90 +3,64 @@ package com.example.cooking_app.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.bumptech.glide.Glide;
 import com.example.cooking_app.Model.Category;
 import com.example.cooking_app.R;
 
+import android.content.Context;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 
-public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
-    ArrayList<Category> categories;
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.RecipeViewHolder> {
 
-    public CategoryAdapter(ArrayList<Category> categories) {
+    private Context context;
+    private ArrayList<Category> categories;
+
+    public CategoryAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setData(ArrayList<Category> categories)
+    {
         this.categories = categories;
+        notifyDataSetChanged();
     }
 
     @NonNull
     @Override
-    public CategoryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_category,parent,false);
-        return new ViewHolder(inflate);
+    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_categories_item, parent, false);
+        return new  RecipeViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CategoryAdapter.ViewHolder holder, int position) {
-        holder.categoryName.setText(categories.get(position).getTitle());
-        String imageUrl = "";
-        switch (position){
-            case 0:{
-                imageUrl = "dessert_orange_food_chocolate";
-                break;
-            }
-            case 1:{
-                imageUrl = "dessert_orange_food_chocolate";
-                break;
-            }
-            case 2:{
-                imageUrl = "dessert_orange_food_chocolate";
-                break;
-            }
-            case 3:{
-                imageUrl = "dessert_orange_food_chocolate";
-                break;
-            }
-            case 4:{
-                imageUrl = "dessert_orange_food_chocolate";
-                break;
-            }
-            case 5:{
-                imageUrl = "dessert_orange_food_chocolate";
-                break;
-            }
-            case 6:{
-                imageUrl = "dessert_orange_food_chocolate";
-                break;
-            }
-            case 7:{
-                imageUrl = "dessert_orange_food_chocolate";
-                break;
-            }
+    public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
+        Category category = categories.get(position);
+        if (category == null)
+        {
+            return;
         }
-        int drawableResourceId = holder.itemView.getContext().getResources().getIdentifier(imageUrl,"drawable",holder.itemView.getContext().getPackageName());
-        Glide.with(holder.itemView.getContext()).load(drawableResourceId).into(holder.categoryImage);
+        holder.nameCategory.setText(category.getTitle());
     }
 
     @Override
     public int getItemCount() {
-        return categories.size();
+        if (categories != null)
+            return categories.size();
+        return 0;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView categoryName;
-        ImageView categoryImage;
-        ConstraintLayout mainLayout;
-        public ViewHolder(@NonNull View itemView) {
+    public class RecipeViewHolder extends RecyclerView.ViewHolder {
+        private TextView nameCategory;
+
+
+        public RecipeViewHolder(@NonNull View itemView) {
             super(itemView);
-            categoryName = itemView.findViewById(R.id.categoryName);
-            categoryImage = itemView.findViewById(R.id.categoryImage);
-            mainLayout = itemView.findViewById(R.id.mainLayout);
+            nameCategory = itemView.findViewById(R.id.categoryName);
         }
     }
 }
